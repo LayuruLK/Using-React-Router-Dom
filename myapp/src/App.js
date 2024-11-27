468c65b4ed8aebedc0c45a28b2ee78cae7ea3a11
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2024-11-27 18:36:30 +0530

    Create Navigation List

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link, NavLink } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav>
+          <ul>
+            <li><Link to='/home'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/contact'>Contact Us</Link></li>
+            <li><Link to='/login'>Login</Link></li>
+          </ul>
+        </nav>
+      </Router>
     </div>
   );
 }
