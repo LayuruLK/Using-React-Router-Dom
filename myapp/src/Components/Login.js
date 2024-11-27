2fb3f15a534892fdbdc6bf046960cc770c6d59d8
LayuruLK <layurulakvidukarunathilaka@gmail.com>
2024-11-27 18:46:03 +0530

    Create Simple Login Form

--- myapp/src/Components/Login.js
+++ myapp/src/Components/Login.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        username:'',
+        password:''
+    });
+
+    const handleChange = (e) => {
+
+    }
+
+    const handleSubmit = () => {
+
+    }
   return (
-    <div>
-      
+    <div className='login'>
+      <div className='login-form'>
+        <form onSubmit={handleSubmit}>
+            <input
+                type='text'
+                name='username'
+                value={formData.username}
+                placeholder='Enter Username'
+                onChange={handleChange}
+            />
+
+            <input
+                type='password'
+                name='password'
+                value={formData.password}
+                placeholder='Enter Password'
+                onChange={handleChange}
+            />
+
+            <button type='submit'>Login</button>
+        </form>
+      </div>
     </div>
   )
 }
